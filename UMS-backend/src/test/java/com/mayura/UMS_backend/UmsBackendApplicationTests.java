a7adc0e07ac7589ad8bcc3ae34c1eacf73d5a824
Mayura Pabasara <mayurapabasara143@gmail.com>
2026-01-11 08:44:57 +0530

    chore: initialize UMS backend with Spring Boot and Maven setup

---
+++ UMS-backend/src/test/java/com/mayura/UMS_backend/UmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayura.UMS_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
